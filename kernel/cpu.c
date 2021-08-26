d8ddea2c6349ba1e300f44d5887da520cda789ec
ZeruxMiatoll <zeruxmiatoll@gmail.com>
2021-08-26 19:14:50 +0200

    patch

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1085,14 +1085,7 @@ static int do_cpu_down(unsigned int cpu, enum cpuhp_state target)
 	int err;
 
 	preempt_disable();
-	cpumask_andnot(&newmask, cpu_online_mask, cpumask_of(cpu));
 	preempt_enable();
-
-	/* One big, LITTLE, and prime CPU must remain online */
-	if (!cpumask_intersects(&newmask, cpu_lp_mask) ||
-	    !cpumask_intersects(&newmask, cpu_perf_mask) ||
-	    !cpumask_intersects(&newmask, cpu_prime_mask))
-		return -EINVAL;
 
 	/*
 	 * When cpusets are enabled, the rebuilding of the scheduling
